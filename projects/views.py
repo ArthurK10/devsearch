fe8dd67d700a5a610c04995288e3af351c788b00
ArthurK10 <arthurkogan10@gmail.com>
2024-01-07 15:09:08 +0200

    added forms

--- projects/views.py
+++ projects/views.py
@@ -1,34 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'SolarStream',
-        'description': "SolarStream is an initiative focused on harnessing solar energy for sustainable water purification."
-    },
-    {
-        'id': '2',
-        'title': 'GreenScape',
-        'description': "GreenScape is an urban greening project that aims to transform cityscapes by integrating vertical gardens."
-    },
-    {
-        'id': '3',
-        'title': 'EduTech Bridge',
-        'description': "EduTech Bridge is an educational technology project aimed at bridging the digital divide in education."
-    },
-]
+from .models import Project
+from .forms import ProjectForm
 
 # Create your views here.
 def projects(request):
-    msg = 'brown recluse'
-    num = 9
-    context = {'massage': msg, 'number': num, 'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
+    projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
